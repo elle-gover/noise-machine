7ab6de4d304b0c7745f94bf32e610a1a5ec5c22f
Elle Gover <elle.gover@detroitlabs.com>
2018-09-05 10:19:50 -0400

    initial commit

---
+++ noiseMachine/ViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ViewController.swift
+//  noiseMachine
+//
+//  Created by Elle Gover on 9/5/18.
+//  Copyright © 2018 com.detroitlabs. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+
+    var audioPlayer: AVAudioPlayer?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func glassButton(_ sender: UIButton) {
+        
+        guard let url = Bundle.main.url(forResource: "glassBreaking", withExtension: "mp3") else { return }
+        
+        do {
+            try AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategoryPlayback)
+            try AVAudioSession.sharedInstance().setActive(true)
+            
+            audioPlayer = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
+            
+            guard let player = audioPlayer else { return }
+            
+            player.play()
+            
+        } catch let error {
+            print("There is an error.")
+        }
+        
+    }
+    
+
+
+}
+
